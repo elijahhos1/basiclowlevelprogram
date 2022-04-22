3486f0062d5303d8ed7f13c7784d07531e913139
elijahhos1 <elijahhos1@gmail.com>
2022-04-22 18:36:33 -0500

    final

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I wasn't sure whether we should start on the project considering " +
+                "we haven't fully confirmed what frameworks to use.\n");
+
+        System.out.println("My apologies if this isn't what I should be doing.");
+    }
+}
